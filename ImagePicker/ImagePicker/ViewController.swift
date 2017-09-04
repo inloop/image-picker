98951232998f8ebd55650d5b6b10a6c37e5faeba
Peter Stajger <stajger.p@gmail.com>
2017-09-04 11:41:42 +0200

    added empty project with framework set up

---
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Peter Stajger on 04/09/2017.
+//  Copyright © 2017 Inloop. All rights reserved.
+//
+
+import UIKit
+import Framework
+
+class ImagePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImagePicker/SourceFiles/ImagePickerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImagePickerViewController.swift
+//  ImagePicker
+//
+//  Created by Peter Stajger on 04/09/2017.
+//  Copyright © 2017 Inloop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class ImagePickerViewController : UICollectionViewController {
+
+}
