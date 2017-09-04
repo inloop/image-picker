89ea1dd61ed0b3f5873c277f2487bfc4298be6fc
Peter Stajger <peter.stajger@inloop.eu>
2017-09-04 15:08:48 +0200

    added example app basic app logic

--- ExampleApp/ExampleApp/ViewController.swift
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -9,20 +9,41 @@
 import UIKit
 import ImagePicker
 
-class ViewController: ImagePickerViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        ImageModel.sayHello()
-        sayHi()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func presentPickerModally() {
+        print("presenting modally")
     }
+    
+}
 
-
+extension ViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = "Presented view controller"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0: presentPickerModally()
+        default: fatalError("not implemented")
+        }
+    }
+    
 }
 
