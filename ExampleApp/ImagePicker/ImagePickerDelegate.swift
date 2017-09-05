e9eaf708c769fc3dadd10d3fdccc1e73b1ea51c3
Peter Stajger <peter.stajger@inloop.eu>
2017-09-05 13:40:27 +0200

    small refactoring

--- ExampleApp/ImagePicker/ImagePickerDelegate.swift
+++ ExampleApp/ImagePicker/ImagePickerDelegate.swift
@@ -19,14 +19,22 @@ struct LayoutConfiguration {
         return showsFirstActionItem || showsSecondActionItem
     }
     
+    ///
+    /// Spacing between items within a section
+    ///
     var interitemSpacing: CGFloat = 1
     
-    static var defaultConfiguration = LayoutConfiguration(
-        showsFirstActionItem: true,
-        showsSecondActionItem: true,
-        showsCameraActionItem: true,
-        interitemSpacing: 2
-    )
+    ///
+    /// Spacing between actions section and camera section
+    ///
+    var actionSectionSpacing: CGFloat = 1
+    
+    ///
+    /// Spacing between camera section and assets section
+    ///
+    var cameraSectionSpacing: CGFloat = 10
+    
+    static var defaultConfiguration = LayoutConfiguration()
 }
 
 final class ImagePickerDelegate : NSObject, UICollectionViewDelegateFlowLayout {
@@ -35,22 +43,25 @@ final class ImagePickerDelegate : NSObject, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
+        guard let layout = collectionViewLayout as? UICollectionViewFlowLayout else {
+            fatalError("currently only UICollectionViewFlowLayout is supported")
+        }
+        
         func sizeForItemInRow(numberOfItemsInRow: Int, preferredWidth: CGFloat?) -> CGSize {
             
-            //horizontal layout
-            let scrollsHorizontally = collectionView.frame.width >= collectionView.frame.height
-            if scrollsHorizontally {
-
+            switch layout.scrollDirection {
+            case .horizontal:
                 var itemHeight = collectionView.frame.height
                 itemHeight -= (collectionView.contentInset.top + collectionView.contentInset.bottom)
                 itemHeight -= (CGFloat(numberOfItemsInRow) - 1) * layoutConfiguration.interitemSpacing
                 itemHeight /= CGFloat(numberOfItemsInRow)
                 itemHeight = floor(itemHeight)
-                
                 return CGSize(width: preferredWidth ?? itemHeight, height: itemHeight)
+                
+            case .vertical:
+                fatalError("unsupported scroll direction")
             }
-            
-            return .zero
+
         }
         
         //we dont care about assets count here
@@ -61,42 +72,6 @@ final class ImagePickerDelegate : NSObject, UICollectionViewDelegateFlowLayout {
         case 1: return sizeForItemInRow(numberOfItemsInRow: layoutModel.numberOfItems(in: 1), preferredWidth: 200)
         case 2: return sizeForItemInRow(numberOfItemsInRow: 3, preferredWidth: nil)
         default: fatalError("unexpected sections count")
-        }
-        
-        
-//        func indexPathIsActionItem(indexPath: IndexPath) -> Bool {
-//            
-//        }
-//        func sizeForItem(at indexPath: IndexPath) -> CGSize {
-//            
-//        }
-        
-        //based on collection view frame we decide if we use vertical of horizontal layout,
-        //for now we use only horizontal
-        
-//        switch indexPath.row {
-//        case 1:
-//            let itemsInFirstRow =
-//            return sizeForItemInRow(numberOfItemsInRow: <#T##Int#>, preferredWidth: <#T##CGFloat#>)
-//        case 2:
-//        case 3:
-//        default:
-//        }
-//        
-//        switch (showsFirstActionItem, showsSecondActionItem, showsCameraActionItem) {
-//        case (true, true, true):
-//            
-//        default:
-//            fatalError("other layouts not implemented")
-//        }
-        
-        let width = collectionView.frame.width - 8
-        //lets assume we have all sections
-        switch indexPath.row {
-        case 0...1: return CGSize(width: width/2 - 4, height: width/2)
-        case 2: return CGSize(width: width, height: width)
-        case 3: return CGSize(width: 100, height: 100)
-        default: return CGSize(width: 100, height: 100)
         }
         
     }
