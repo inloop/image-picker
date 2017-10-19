fda4a196d08cbbc4d8b93f330970fd8b015ed988
Peter Stajger <stajger.p@gmail.com>
2017-10-19 11:49:17 +0200

    changed default background color to match original iMessage app

--- ImagePicker/Appearance.swift
+++ ImagePicker/Appearance.swift
@@ -15,5 +15,5 @@ public class Appearance {
     ///
     /// Image picker background color.
     ///
-    public var backgroundColor: UIColor = UIColor.red
+    public var backgroundColor: UIColor = UIColor(red: 208/255, green: 213/255, blue: 218/255, alpha: 1)
 }
